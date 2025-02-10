58bd21e733ef28a82716fccb5aac7d0a37a5f58d
RoyYeah <rodrigoacamargog@gmail.com>
2025-02-10 19:16:21 -0400

    feat: enhance game grid by updating square creation logic and styling

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const juego = document.querySelector('#juego')
 
-for (let i = 0; i <= 16; i++) {
+for (let i = 1; i <= 16*16; i++) {
     const squares = document.createElement("div");
     squares.id = "squares";
+    squares.innerHTML = i;
     juego.appendChild(squares);
     console.log(i)
 }
