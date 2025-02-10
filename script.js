5555e1e149ea0bdff034042c990d89d3237e6f52
RoyYeah <rodrigoacamargog@gmail.com>
2025-02-10 19:57:44 -0400

    feat: implement hover effect for game squares and clean up HTML structure

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const juego = document.querySelector('#juego')
+const juego = document.querySelector('#juego');
+const hover = document.querySelector(".hovered");
 
+function color() {
+    return Math.floor(Math.random() * 256);
+};
 for (let i = 1; i <= 16*16; i++) {
     const squares = document.createElement("div");
-    squares.id = "squares";
+    squares.classList.add("squares");
+    squares.addEventListener('mouseover', () => {
+        squares.classList.remove("squares");
+        squares.classList.add("hovered");
+    });
+    squares.addEventListener('mouseout', () => {
+        squares.classList.remove("hovered");
+        squares.classList.add("squares");
+    });
     squares.innerHTML = i;
     juego.appendChild(squares);
-    console.log(i)
-}
+};
