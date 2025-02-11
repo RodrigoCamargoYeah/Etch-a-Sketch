c18b719ce91c285da702713454c57e7d7e0d53be
RoyYeah <rodrigoacamargog@gmail.com>
2025-02-11 00:11:59 -0400

    feat: refactor square creation and hover functionality for improved grid interaction

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const juego = document.querySelector('#juego');
-const hover = document.querySelector(".hovered");
 
-function color() {
-    return Math.floor(Math.random() * 256);
-};
-for (let i = 1; i <= 16*16; i++) {
-    const squares = document.createElement("div");
-    squares.classList.add("squares");
-    squares.addEventListener('mouseover', () => {
+function hoverSquares(squares) {
+    squares.addEventListener("mouseover", () => {
         squares.classList.remove("squares");
         squares.classList.add("hovered");
-    });
-    squares.addEventListener('mouseout', () => {
+    })
+    squares.addEventListener("mouseout", () => {
         squares.classList.remove("hovered");
         squares.classList.add("squares");
-    });
-    squares.innerHTML = i;
+    })
+}
+
+function generateSquare() {
+    const squares = document.createElement("div");
+    squares.classList.add("squares");
+    hoverSquares(squares);
     juego.appendChild(squares);
-};
+}
+
+function createGrid(dimension) {
+    for (let i = 0; i < dimension; i++) {
+        generateSquare();
+    }
+}
+
+createGrid(256);
