7cfce486dc96815c262b071e87b2fe52b6cc02b6
RoyYeah <rodrigoacamargog@gmail.com>
2025-02-11 12:02:07 -0400

    feat: add button to dynamically create game grid with user-defined size

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const juego = document.querySelector('#juego');
+const btn = document.querySelector('button');
 
 function hoverSquares(squares) {
     squares.addEventListener("mouseover", () => {
@@ -19,9 +20,21 @@ function generateSquare() {
 }
 
 function createGrid(dimension) {
-    for (let i = 0; i < dimension; i++) {
+    for (let i = 0; i < dimension * dimension; i++) {
         generateSquare();
     }
 }
 
-createGrid(256);
+btn.addEventListener('click', () => {
+    let gridSize = parseInt(prompt('numero'));
+    while (juego.firstChild) {
+        juego.removeChild(juego.firstChild);
+    }
+    if (gridSize > 100) {
+        gridSize = 100;
+    }
+    createGrid(gridSize);
+})
+
+createGrid(16);
+
